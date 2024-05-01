b6298d72b231ae20144cc5b8f3baf39d6d00ab34
alexismann <akm4rp@virginia.edu>
2024-05-01 10:17:36 -0400

    ble working, not sending # in ad data

--- src/main.c
+++ src/main.c
@@ -10,16 +10,133 @@
 
 #include <dk_buttons_and_leds.h>
 
+#include <stdbool.h>
+#include <zephyr/types.h>
+#include <zephyr/drivers/sensor.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/dt-bindings/gpio/gpio.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+
 #define MAX_REC_COUNT		3
 #define NDEF_MSG_BUF_SIZE	128
 
 #define NFC_FIELD_LED		DK_LED1
+
+#define LAB2_SERVICE_UUID BT_UUID_128_ENCODE(0x5253FF4B, 0xE47C, 0x4EC8, 0x9792, 0x69FDF4923B4A)
+
+static ssize_t characteristic_read(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset);
+
+uint32_t characteristic_value = 0x0;
+
+// Set up the advertisement data.
+#define DEVICE_NAME "group2"
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+//BDFC9792-8234-405E-AE02-35EF4174B299
+#define NEW_SERVICE_UUID BT_UUID_128_ENCODE(0xBDFC9792, 0x8234, 0x405E, 0xAE02, 0x35EF4174B299)
+#define CUSTOM_SERVICE_UUID BT_UUID_128_ENCODE(0xBDFC9792, 0x8234, 0x405E, 0xAE02, 0x35EF4174B299)
 
 static uint8_t en_payload[5]; // Changed to 3 bytes
 static const uint8_t en_code[] = {'e', 'n'};
 
 /* Buffer used to hold an NFC NDEF message. */
 static uint8_t ndef_msg_buf[NDEF_MSG_BUF_SIZE];
+
+static const struct bt_data ad[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, NEW_SERVICE_UUID)
+};
+
+
+
+// Setup the the service and characteristics.
+BT_GATT_SERVICE_DEFINE(lab2_service,
+	BT_GATT_PRIMARY_SERVICE(
+		BT_UUID_DECLARE_128(LAB2_SERVICE_UUID)
+	),
+	BT_GATT_CHARACTERISTIC(BT_UUID_DECLARE_16(0x000a), BT_GATT_CHRC_READ,
+			       BT_GATT_PERM_READ, characteristic_read, NULL, &characteristic_value),
+);
+
+// Setup callbacks when devices connect and disconnect.
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err 0x%02x)\n", err);
+	} else {
+		printk("Connected\n");
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+
+static ssize_t characteristic_read(struct bt_conn *conn,
+			                       const struct bt_gatt_attr *attr,
+								   void *buf,
+			                       uint16_t len,
+								   uint16_t offset)
+{
+	// The `user_data` corresponds to the pointer provided as the last "argument"
+	// to the `BT_GATT_CHARACTERISTIC` macro.
+	uint32_t *value = attr->user_data;
+
+	// Need to encode data into a buffer to send to client.
+	uint8_t out_buffer[4] = {0};
+
+	out_buffer[0]=0xc5;
+	out_buffer[1]=0xec;
+	out_buffer[2]=0x45;
+	out_buffer[3]=0x01;
+
+	// User helper function to encode the output data to send to
+	// the client.
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, out_buffer, 4);
+}
 
 /* Function to generate a random number */
 static int generateRandomNumber() {
@@ -109,6 +226,14 @@ int main(void)
 
     /* Generate random number */
     int randomNumber = generateRandomNumber();
+
+	int err;
+
+	err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
 
     /* Encode welcome message with the generated random number */
     if (welcome_msg_encode(ndef_msg_buf, &len, randomNumber) < 0) {
